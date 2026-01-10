f9cef5dca13273c0cbb5dc17fbc8a613663b12e4
faheem mekrani <faheemmekrani@pal-nat186-157-108.itap.purdue.edu>
2026-01-10 16:51:42 -0500

    added backend

--- fuel/fuelApp.swift
+++ fuel/fuelApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct fuelApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
